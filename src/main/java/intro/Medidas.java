93a222b1bbedae1a8de807206a156c48bad3d74d
karla <karlayasmine@gmail.com>
2022-02-04 14:15:34 -0300

    Excli o "Bom dia" no começo da impressão de texto no Console Criei o método calcularAreaModoCurto e movi o código que estava em main para lá
    O método main agora chama os dois métodos calcular* que passaram também a ser static

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -9,8 +9,14 @@ public class Medidas {
 
     // 3.2 - Métodos e Funções
     public static void main(String[] args) {
-        System.out.println("Bom Dia!!!");
-        System.out.println("Calculo de Areas");
+
+        calcularAreaModoCurto();
+        calcularAreaModoExtenso();
+
+    }
+
+    public static void calcularAreaModoCurto() {
+        System.out.println("Calculo de Areas Modo Curto");
 
         // Calculo de área - Reduzido
         int largura = 4;
@@ -18,10 +24,12 @@ public class Medidas {
 
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + largura * comprimento + "m²");
-
     }
 
-    public void calcularAreaModoExtenso() {
+    public static void calcularAreaModoExtenso() {
+
+        System.out.println("Calculo de Areas Modo Extenso");
+
         // Calculo de área - Exemplo: o tamanho do tapete ou do piso
         int largura;
         int comprimento;
@@ -33,18 +41,8 @@ public class Medidas {
         resultado = largura * comprimento;
         System.out.println("Para a largura de " + largura + "m e o comprimento de "
                 + comprimento + "m a área é de " + resultado + " m²");
+
     }
-
-    public void calcularAreaModoCompacto(){
-            // Calculo de área - Reduzido
-        int largura = 4;
-        int comprimento = 3;
-
-            System.out.println("Para a largura de " + largura + "m e o comprimento de "
-                    + comprimento + "m a área é de " + largura * comprimento + "m²");
-    }
-
 }
-
 
 
